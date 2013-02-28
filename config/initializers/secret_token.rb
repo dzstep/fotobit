28ee58a1b531ebaa1d7ea4eaabe9616c60faf142
David Stepania <dzstep@gmail.com>
2013-02-28 03:26:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fotobit::Application.config.secret_token = '70d0d7f77cdc4ac3faf9f68812b6c259a828fe52b8e9e5e47070096753869024c17490c4a1093830c5df33c22467ae86158ab5ca039e0ddb569a50eeebc26415'
